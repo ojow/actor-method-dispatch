86336389a35d291dffac7763c5032012f60ce8ed
ojow <ojowoo@gmail.com>
2014-09-23 18:13:23 +0700

    publishing

--- project/ActorMethodDispatch.scala
+++ project/ActorMethodDispatch.scala
@@ -1,17 +1,22 @@
 import sbt._
 import Keys._
+import xerial.sbt.Sonatype._
+import SonatypeKeys._
 import com.typesafe.sbt.SbtMultiJvm
 import com.typesafe.sbt.SbtMultiJvm.MultiJvmKeys.MultiJvm
 
 
-object ActorMethodDispatchBuild extends Build {
+object ActorMethodDispatch extends Build {
    val akkaVersion = "2.3.6"
 
-   lazy val main = Project("main", file("."), settings = SbtMultiJvm.multiJvmSettings ++ Seq(
+   lazy val main = Project("main", file("."), settings = SbtMultiJvm.multiJvmSettings ++ sonatypeSettings ++ Seq(
      scalaVersion in Global := "2.11.2",
      organization in Global := "net.ogalako",
      version in Global := "0.2-SNAPSHOT",
      name := "actor-method-dispatch",
+     licenses := Seq("Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0.html")),
+     homepage := Some(url("https://github.com/ojow/actor-method-dispatch")),
+     profileName := "ojow",
      libraryDependencies ++= Seq(
        "com.typesafe.akka" %% "akka-remote" % akkaVersion % "test",
        "com.typesafe.akka" %% "akka-multi-node-testkit" % akkaVersion % "test",
@@ -37,7 +42,28 @@ object ActorMethodDispatchBuild extends Build {
      mappings in (Compile, packageBin) ++= mappings.in(commonSub, Compile, packageBin).value,
      mappings in (Compile, packageSrc) ++= mappings.in(commonSub, Compile, packageSrc).value,
 
-     publishMavenStyle := true
+     publishMavenStyle := true,
+     pomExtra := {
+       <url>https://github.com/ojow/actor-method-dispatch</url>
+         <licenses>
+           <license>
+             <name>Apache 2</name>
+             <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+           </license>
+         </licenses>
+         <scm>
+           <connection>scm:git:github.com/ojow/actor-method-dispatch.git</connection>
+           <developerConnection>scm:git:git@github.com:ojow/actor-method-dispatch.git</developerConnection>
+           <url>github.com/ojow/actor-method-dispatch.git</url>
+         </scm>
+         <developers>
+           <developer>
+             <id>ojow</id>
+             <name>Oleg Galako</name>
+             <url>https://github.com/ojow</url>
+           </developer>
+         </developers>
+     }
    )) dependsOn(macroSub, commonSub) aggregate(macroSub, commonSub) configs (MultiJvm)
 
    lazy val commonSub = Project("common", file("common")) settings(
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,4 +2,6 @@ addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-multi-jvm" % "0.3.8")
 
+addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "0.2.1")
+
 addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.3")
